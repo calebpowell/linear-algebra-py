175d5b852d8fe3e1596ecb631980131d5fd62d4f
Caleb Powell <caleb.powell+github@gmail.com>
2016-01-15 01:31:39 -0500

    Lesson 1
    Added initial vector class and implemented +, -, and scalar multiplication.

---
+++ test_vector.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from vector import Vector
+
+__author__ = 'caleb'
+
+
+class TestVector(TestCase):
+    def test_add(self):
+        v1 = Vector([8.218, -9.341])
+        v2 = Vector([-1.129, 2.111])
+
+        self.assertEqual(v1.plus(v2), Vector([7.089, -7.229999999999999]))
+        self.assertEqual(v2.plus(v1), Vector([7.089, -7.229999999999999]))
+
+    def test_subtract(self):
+        v1 = Vector([7.119, 8.215])
+        v2 = Vector([-8.223, 0.878])
+
+        self.assertEqual(v1.minus(v2), Vector([15.342, 7.337]))
+
+    def test_scalar_multiplication_by_scalar(self):
+        v = Vector([1.671, -1.012, -0.318])
+        self.assertEqual(v.times_scalar(7.41), Vector([12.38211, -7.49892, -2.35638]))
---
+++ vector.py
@@ -0,0 +1,35 @@
+import operator
+__author__ = 'caleb'
+
+
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def plus(self, vector):
+        new_coordinates = [x+y for x,y in zip(self.coordinates, vector.coordinates)]
+        return Vector(new_coordinates)
+
+    def minus(self, vector):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, vector.coordinates)]
+        return Vector(new_coordinates)
+
+    def times_scalar(self, c):
+        new_coordinates = [c * x for x in self.coordinates]
+        return Vector(new_coordinates)
